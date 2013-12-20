1d5b8bbfa295df59d20a3729473c1e73cd0ad7b2
Kevin Robayna <k3vinaso@gmail.com>
2013-12-20 17:41:09 +0000

    added script to setup

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#****************************************************
+#                 Description                       *
+#                                                   *
+#  Script for setup of Vim and Bash Configuration   *
+#                                                   *
+#           Author: Kevin Robayna                   *
+#                                                   *
+#****************************************************
+
+mv .bash_aliases ~/
+mv .bash_profile ~/  
+mv .bash_prompt ~/
+mv .bashrc ~/  
+mv .vimrc ~/ 
+mv -r .bash_scripts ~/
+mv -r .ipython ~/
+mv -r .vim ~/
+
