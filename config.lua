5c8b6b77ad0d5478efbb177fc43c16fd361ccad7
Kevin Robayna <me@kevinrobayna.com>
2023-02-09 13:23:04 +0000

    delete iterm stuff

--- config.lua
+++ config.lua
@@ -34,9 +34,9 @@ vim.opt.colorcolumn = "120"
 -- general
 lvim.log.level = "info"
 lvim.format_on_save = {
-  enabled = true,
-  pattern = "*.lua",
-  timeout = 1000,
+    enabled = true,
+    pattern = "*.lua",
+    timeout = 1000,
 }
 
 -- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
@@ -52,35 +52,24 @@ lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Project
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
-  -- for input mode
-  i = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
-  },
-  -- for normal mode
-  n = {
-    ["<c-j>"] = actions.move_selection_next,
-    ["<c-k>"] = actions.move_selection_previous,
-  },
+    -- for input mode
+    i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+    },
+    -- for normal mode
+    n = {
+        ["<c-j>"] = actions.move_selection_next,
+        ["<c-k>"] = actions.move_selection_previous,
+    },
 }
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 
 
-------------------------
--- Harpoon
-------------------------
-lvim.keys.normal_mode["<leader>a"] = require("harpoon.mark").add_file
-lvim.keys.normal_mode["<C-e>"] = require("harpoon.ui").toggle_quick_menu
-
-lvim.keys.normal_mode["<C-h>"] = function() require("harpoon.ui").nav_file(1) end
-lvim.keys.normal_mode["<C-j>"] = function() require("harpoon.ui").nav_file(2) end
-lvim.keys.normal_mode["<C-k>"] = function() require("harpoon.ui").nav_file(3) end
-lvim.keys.normal_mode["<C-l>"] = function() require("harpoon.ui").nav_file(4) end
-
 -- Change theme settings
-lvim.colorscheme = "lunar"
+lvim.colorscheme = "catppuccin-mocha"
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerSync
 lvim.builtin.alpha.active = true
@@ -107,34 +96,46 @@ linters.setup {
 
 -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-  {
-    "sindrets/diffview.nvim",
-    event = "BufRead",
-  },
-  {
-    "f-person/git-blame.nvim",
-    event = "BufRead",
-    config = function()
-      vim.cmd "highlight default link gitblame SpecialComment"
-      vim.g.gitblame_enabled = 0
-    end,
-  },
-  { "ThePrimeagen/harpoon" }
+    {
+        "sindrets/diffview.nvim",
+        event = "BufRead",
+    },
+    {
+        "f-person/git-blame.nvim",
+        event = "BufRead",
+        config = function()
+          vim.cmd "highlight default link gitblame SpecialComment"
+          vim.g.gitblame_enabled = 0
+        end,
+    },
+    { "ThePrimeagen/harpoon" },
+    { "catppuccin/nvim" }
 }
 
 -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "zsh",
-  callback = function()
-    -- let treesitter use bash highlight for zsh files as well
-    require("nvim-treesitter.highlight").attach(0, "bash")
-  end,
+    pattern = "zsh",
+    callback = function()
+      -- let treesitter use bash highlight for zsh files as well
+      require("nvim-treesitter.highlight").attach(0, "bash")
+    end,
 })
 
+
+------------------------
+-- Harpoon
+------------------------
 local harpoon = require "harpoon"
 require("telescope").load_extension('harpoon')
 harpoon.setup {
-  menu = {
-    width = vim.api.nvim_win_get_width(0) - 4,
-  }
+    menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+    }
 }
+lvim.keys.normal_mode["<leader>a"] = require("harpoon.mark").add_file
+lvim.keys.normal_mode["<C-e>"] = require("harpoon.ui").toggle_quick_menu
+
+lvim.keys.normal_mode["<C-h>"] = function() require("harpoon.ui").nav_file(1) end
+lvim.keys.normal_mode["<C-j>"] = function() require("harpoon.ui").nav_file(2) end
+lvim.keys.normal_mode["<C-k>"] = function() require("harpoon.ui").nav_file(3) end
+lvim.keys.normal_mode["<C-l>"] = function() require("harpoon.ui").nav_file(4) end
