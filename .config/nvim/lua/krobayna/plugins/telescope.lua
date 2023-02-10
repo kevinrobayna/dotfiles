1f21e5f1d5b2b91801e7a0cb3ba68cbd80d7b2e2
Kevin Robayna <me@kevinrobayna.com>
2023-02-10 11:38:27 +0000

    update config for telescope and treessiter

--- .config/nvim/lua/krobayna/plugins/telescope.lua
+++ .config/nvim/lua/krobayna/plugins/telescope.lua
@@ -16,6 +16,11 @@ if not themes_setup then
 	return
 end
 
+-- import telescope-ui-select safely
+local builtin_setup, builtin = pcall(require, "telescope.builtin")
+if not builtin_setup then
+	return
+end
 -- configure telescope
 telescope.setup({
 	-- configure custom mappings
@@ -37,3 +42,20 @@ telescope.setup({
 
 telescope.load_extension("fzf")
 telescope.load_extension("ui-select")
+
+local keymap = vim.keymap -- for conciseness
+keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
+keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+keymap.set("n", "<leader>/", function()
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	builtin.current_buffer_fuzzy_find(themes.get_dropdown({
+		winblend = 10,
+		previewer = false,
+	}))
+end, { desc = "[/] Fuzzily search in current buffer]" })
+
+keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
