4ed45310e270ecf1076dc910dab16412514bde6b
Kevin Robayna <me@kevinrobayna.com>
2023-03-30 09:23:05 +0100

    improve auto completes

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,5 +1,4 @@
 return {
-
 	-- snippets
 	{
 		"L3MON4D3/LuaSnip",
@@ -23,11 +22,9 @@ return {
         function()
           return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
         end,
-        expr = true,
-        silent = true,
-        mode = "i",
+        expr = true, silent = true, mode = "i",
       },
-      { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s" },
+      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
       { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
     },
 	},
