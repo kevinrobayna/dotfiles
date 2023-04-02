71e7312920d219cd2bbaf6fec794d8bfc5764cb5
Kevin Robayna <me@kevinrobayna.com>
2023-04-02 18:55:22 +0100

    update git config

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"f-person/git-blame.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
+		},
+	},
+}
