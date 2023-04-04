b14ca96808eb9a8936b691f6a0b645d371333645
Kevin Robayna <me@kevinrobayna.com>
2023-04-04 12:55:57 +0100

    add keybinding to open files in github

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -42,4 +42,19 @@ return {
 			},
 		},
 	},
+	{
+		"tyru/open-browser-github.vim",
+		dependencies = {
+			"tyru/open-browser.vim",
+		},
+		cmd = {
+			"OpenGithubFile",
+			"OpenGithubIssue",
+			"OpenGithubPullReq",
+			"OpenGithubProject",
+		},
+		keys = {
+			{ "<leader>co", "<Cmd>OpenGithubFile<CR>", desc = "Open in Github" },
+		},
+	},
 }
