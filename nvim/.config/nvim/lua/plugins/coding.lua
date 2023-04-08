390c2683be58d6732841072da82e81d621f76412
Kevin Robayna <me@kevinrobayna.com>
2023-04-08 22:53:54 +0100

    use another plugin for comments

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -191,47 +191,31 @@ return {
 		end,
 	},
 	{
-		"numToStr/Comment.nvim",
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
 		dependencies = {
 			"JoosepAlviste/nvim-ts-context-commentstring",
 		},
-		keys = {
-			{
-				"<leader>/",
-				"<cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
-				desc = "Toggle Comment",
-			},
-			{
-				"<leader>/",
-				"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-				mode = { "x" },
-				desc = "Toggle Comment",
-			},
-		},
 		opts = {
-			pre_hook = function(ctx)
-				-- Only calculate commentstring for tsx filetypes
-				if vim.bo.filetype == "typescriptreact" then
-					local U = require("Comment.utils")
+			hooks = {
+				pre = function()
+					require("ts_context_commentstring.internal").update_commentstring({})
+				end,
+			},
+			mappings = {
+				-- Toggle comment (like `gcip` - comment inner paragraph) for both
+				-- Normal and Visual modes
+				comment = "<leader>/",
+				-- Toggle comment on current line
+				comment_line = "<leader>/",
 
-					-- Determine whether to use linewise or blockwise commentstring
-					local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-					-- Determine the location where to calculate commentstring from
-					local location = nil
-					if ctx.ctype == U.ctype.blockwise then
-						location = require("ts_context_commentstring.utils").get_cursor_location()
-					elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-						location = require("ts_context_commentstring.utils").get_visual_start_location()
-					end
-
-					return require("ts_context_commentstring.internal").calculate_commentstring({
-						key = type,
-						location = location,
-					})
-				end
-			end,
+				-- Define 'comment' textobject (like `dgc` - delete whole comment block)
+				textobject = "<leader>/",
+			},
 		},
+		config = function(_, opts)
+			require("mini.comment").setup(opts)
+		end,
 	},
 	{
 		"echasnovski/mini.bufremove",
