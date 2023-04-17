2a60a7365fab032fc60f304ad9fc21dc260e6624
Kevin Robayna <me@kevinrobayna.com>
2023-04-17 15:45:48 +0100

    transparent mode

--- config/.config/nvim/lua/plugins/tokyonight.lua
+++ config/.config/nvim/lua/plugins/tokyonight.lua
@@ -3,6 +3,10 @@ return {
 	lazy = false,
 	priority = 1000,
 	opts = function()
+		require("tokyonight").setup({
+			style = "moon",
+			transparent = true,
+		})
 		vim.cmd([[colorscheme tokyonight]])
 	end,
 }
