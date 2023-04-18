b0dd300386c5fbf11bfb2d2128cde656bdfe94de
Kevin Robayna <me@kevinrobayna.com>
2023-04-18 09:14:41 +0100

    fix nvimtree background to make it transparent

--- config/.config/nvim/lua/plugins/tokyonight.lua
+++ config/.config/nvim/lua/plugins/tokyonight.lua
@@ -6,6 +6,11 @@ return {
 		require("tokyonight").setup({
 			style = "moon",
 			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				-- NOTE: this is an option but i don't think having the floats with a backgrund hurts
+				-- floats = "transparent",
+			},
 		})
 		vim.cmd([[colorscheme tokyonight]])
 	end,
