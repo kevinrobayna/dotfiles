784d8a9bec23e4856cd6add5322fd4bac74be291
Kevin Robayna <me@kevinrobayna.com>
2023-05-02 13:50:45 +0100

    add exports for google sdk back

--- config/.config/zsh/exports.zsh
+++ config/.config/zsh/exports.zsh
@@ -73,3 +73,6 @@ else
 fi
 unset __conda_setup
 # <<< conda initialize <<<
+
+source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
