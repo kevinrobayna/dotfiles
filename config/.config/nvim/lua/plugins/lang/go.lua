7606f23c3a5855d542203b8e31caeb37d3129670
Kevin Robayna <me@kevinrobayna.com>
2023-06-04 22:11:21 +0100

    use stuff from lazyvim

---
+++ config/.config/nvim/lua/plugins/lang/go.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        nls.builtins.formatting.gofmt,
+        nls.builtins.formatting.gofumpt,
+
+        nls.builtins.diagnostics.golangci_lint,
+      })
+    end,
+  },
+}
