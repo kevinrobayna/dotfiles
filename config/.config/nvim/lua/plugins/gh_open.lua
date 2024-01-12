c659258fb6dbcad7d373e4967443aa6f84cd0927
Kevin Robayna <me@kevinrobayna.com>
2024-01-12 11:28:16 +0100

    remove cmds for gh open

--- config/.config/nvim/lua/plugins/gh_open.lua
+++ config/.config/nvim/lua/plugins/gh_open.lua
@@ -3,12 +3,6 @@ return {
   dependencies = {
     "tyru/open-browser.vim",
   },
-  cmd = {
-    "OpenGithubFile",
-    "OpenGithubIssue",
-    "OpenGithubPullReq",
-    "OpenGithubProject",
-  },
   keys = {
     { "<leader>co", "<Cmd>OpenGithubFile<CR>", desc = "Open in Github" },
   },
