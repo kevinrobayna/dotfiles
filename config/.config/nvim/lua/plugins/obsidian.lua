3493fb7204aae6e89c5cbd4f7106b240d653b81f
Kevin Robayna <me@kevinrobayna.com>
2024-01-26 14:15:52 +0000

    allow me to open my notes wherever i am

--- config/.config/nvim/lua/plugins/obsidian.lua
+++ config/.config/nvim/lua/plugins/obsidian.lua
@@ -2,14 +2,9 @@ return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
+  keys = {
+    { "<leader>no", "<Cmd>vsplit<CR><Cmd>ObsidianToday<CR>", desc = "Open Notes" },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
@@ -21,6 +16,14 @@ return {
           name = "Notes",
           path = "~/dev/notes",
         },
+      },
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "diary",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = "%Y-W%W",
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = "weekly.md",
       },
       completion = {
         -- Set to false to disable completion.
