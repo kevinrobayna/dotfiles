311f8ac6ac4158f3a0fedc8cfcb033a65b71d136
Kevin Robayna <me@kevinrobayna.com>
2024-01-29 09:18:01 +0000

    update obsidian config

--- config/.config/nvim/lua/plugins/obsidian.lua
+++ config/.config/nvim/lua/plugins/obsidian.lua
@@ -3,60 +3,69 @@ return {
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   keys = {
-    { "<leader>no", "<Cmd>vsplit<CR><Cmd>ObsidianToday<CR>", desc = "Open Notes" },
+    { "<leader>no", "<Cmd>ObsidianToday<CR>", desc = "Open Notes" },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  opts = {},
-  config = function()
-    require("obsidian").setup({
-      workspaces = {
-        {
-          name = "Notes",
-          path = "~/dev/vault",
-        },
+  opts = {
+    workspaces = {
+      {
+        name = "Notes",
+        path = "~/dev/vault",
       },
-      daily_notes = {
-        -- Optional, if you keep daily notes in a separate directory.
-        folder = "diary",
-        -- Optional, if you want to change the date format for the ID of daily notes.
-        date_format = "%Y-W%W",
-        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-        template = "weekly.md",
-      },
-      completion = {
-        -- Set to false to disable completion.
-        nvim_cmp = true,
+    },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "diary",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-W%W",
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "weekly.md",
+    },
 
-        -- Trigger completion at 2 chars.
-        min_chars = 2,
+    -- Optional, determines how certain commands open notes. The valid options are:
+    -- 1. "current" (the default) - to always open in the current window
+    -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+    -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+    open_notes_in = "vsplit",
 
-        -- Where to put new notes created from completion. Valid options are
-        --  * "current_dir" - put new notes in same directory as the current buffer.
-        --  * "notes_subdir" - put new notes in the default notes subdirectory.
-        new_notes_location = "notes_subdir",
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.fn.jobstart({ "open", url }) -- Mac OS
+    end,
 
-        -- Control how wiki links are completed with these (mutually exclusive) options:
-        --
-        -- 1. Whether to add the note ID during completion.
-        -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-        -- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
-        prepend_note_id = true,
-        -- 2. Whether to add the note path during completion.
-        -- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
-        -- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
-        prepend_note_path = false,
-        -- 3. Whether to only use paths during completion.
-        -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
-        -- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
-        use_path_only = false,
-      },
-      templates = {
-        subdir = "Templates",
-        date_format = "%Y-%m-%d-%a",
-        time_format = "%H:%M",
-      },
-    })
-  end,
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+
+      -- Where to put new notes created from completion. Valid options are
+      --  * "current_dir" - put new notes in same directory as the current buffer.
+      --  * "notes_subdir" - put new notes in the default notes subdirectory.
+      new_notes_location = "notes_subdir",
+
+      -- Control how wiki links are completed with these (mutually exclusive) options:
+      --
+      -- 1. Whether to add the note ID during completion.
+      -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
+      -- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
+      prepend_note_id = true,
+      -- 2. Whether to add the note path during completion.
+      -- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
+      -- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
+      prepend_note_path = false,
+      -- 3. Whether to only use paths during completion.
+      -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
+      -- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
+      use_path_only = false,
+    },
+    templates = {
+      subdir = "Templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+    },
+  },
 }
