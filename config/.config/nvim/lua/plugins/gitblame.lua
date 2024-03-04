59b1173f4414b09716ba2d13bf98dbf193ce2d2b
Kevin Robayna <me@kevinrobayna.com>
2024-03-04 08:43:49 +0000

    change gitblame plugin, virtual text was too slow

--- config/.config/nvim/lua/plugins/gitblame.lua
+++ config/.config/nvim/lua/plugins/gitblame.lua
@@ -1,4 +1,11 @@
 return {
-  "f-person/git-blame.nvim",
+  "FabijanZulj/blame.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    merge_consecutive = true,
+    virtual_style = "float",
+  },
+  keys = {
+    { "<leader>gb", "<cmd>ToggleBlame<cr>", desc = "Toggle Git Blame UI" },
+  },
 }
