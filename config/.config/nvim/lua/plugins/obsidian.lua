567c1d14175f9002a37529c71562b849fbd414e6
Kevin Robayna <me@kevinrobayna.com>
2024-03-22 20:44:48 +0000

    add another plugin to my obsidian config

--- config/.config/nvim/lua/plugins/obsidian.lua
+++ config/.config/nvim/lua/plugins/obsidian.lua
@@ -1,5 +1,21 @@
+vim.cmd([[highlight CodeBlock guibg=none]])
+vim.cmd([[highlight Headline guibg=none]])
+vim.cmd([[highlight Dash guibg=#D19A66 gui=bold]])
 return {
   "preservim/vim-pencil",
+
+  {
+    "lukas-reineke/headlines.nvim",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    ft = "markdown",
+    opts = {
+      markdown = {
+        fat_headlines = false,
+        fat_headline_upper_string = "_",
+        fat_headline_lower_string = "-",
+      },
+    },
+  },
   {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
