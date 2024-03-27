c76f533decc2be33f642aae9f9818a736bd7c835
Kevin Robayna <me@kevinrobayna.com>
2024-03-27 17:30:13 +0000

    use dial from extras

--- config/.config/nvim/lua/config/lazy.lua
+++ config/.config/nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.go" },
