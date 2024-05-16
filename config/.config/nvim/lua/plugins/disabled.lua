ad5b1929b860258cdd1e65b87902a3ce1afb37ed
Kevin Robayna <me@kevinrobayna.com>
2024-05-16 20:49:58 +0100

    remove mini.comment

--- config/.config/nvim/lua/plugins/disabled.lua
+++ config/.config/nvim/lua/plugins/disabled.lua
@@ -2,5 +2,4 @@ return {
   { "akinsho/bufferline.nvim", enabled = false }, -- Not necessary (Harpoon!)
   { "catppuccin", enabled = false },
   { "mini.pairs", enabled = false },
-  { "mini.comment", enabled = false },
 }
