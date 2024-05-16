4dfb453948a9273d06df3e1bef63498c7325d563
Kevin Robayna <me@kevinrobayna.com>
2024-05-16 17:12:47 +0100

    use built-in comment functionality

--- config/.config/nvim/lua/plugins/disabled.lua
+++ config/.config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false }, -- Not necessary (Harpoon!)
   { "catppuccin", enabled = false },
   { "mini.pairs", enabled = false },
+  { "mini.comment", enabled = false },
 }
