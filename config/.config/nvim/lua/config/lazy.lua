9589477fb3e7c019fc1b42d76e9035add11cb976
Kevin Robayna <me@kevinrobayna.com>
2024-05-21 17:50:11 +0100

    add python

--- config/.config/nvim/lua/config/lazy.lua
+++ config/.config/nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.docker" },
