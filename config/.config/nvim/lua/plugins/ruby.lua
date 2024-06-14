5168266c841f4f737ce79fdd643ec8eae3fe50a1
Kevin Robayna <me@kevinrobayna.com>
2024-06-14 16:16:50 +0100

    remove overrides from ruby extra
    Soon i might be able to remove more stuff
    https://github.com/LazyVim/LazyVim/pull/3652

--- config/.config/nvim/lua/plugins/ruby.lua
+++ config/.config/nvim/lua/plugins/ruby.lua
@@ -1,42 +1,19 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "ruby" })
-      end
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, {
-          "ruby-lsp",
-          "sorbet",
-          "rubocop",
-        })
-      end
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
     opts = {
+      ---@type lspconfig.options
       servers = {
-        rubocop = {},
+        -- disable solargraph from auto running when you open ruby files
+        solargraph = {
+          autostart = false,
+        },
+        -- ruby_lsp will be automatically installed with mason and loaded with lspconfig
         ruby_lsp = {},
+        rubocop = {},
         sorbet = {},
       },
-    },
-  },
-  {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    dependencies = {
-      "suketa/nvim-dap-ruby",
-      config = function()
-        require("dap-ruby").setup()
-      end,
     },
   },
   {
