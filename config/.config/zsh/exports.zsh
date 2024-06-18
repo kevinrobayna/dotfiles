b4df63652c915477302d6ff4664cd18ce00636b5
Kevin Robayna <me@kevinrobayna.com>
2024-06-18 15:11:20 +0100

    update fzr theme

--- config/.config/zsh/exports.zsh
+++ config/.config/zsh/exports.zsh
@@ -48,7 +48,29 @@ export LDFLAGS="-L$(eval "brew --prefix")/opt/postgresql@15/lib"
 export CPPFLAGS="-I$(eval "brew --prefix")/opt/postgresql@15/include"
 export PKG_CONFIG_PATH="$(eval "brew --prefix")/opt/postgresql@15/lib/pkgconfig"
 
-source ~/.local/share/nvim/lazy/tokyonight.nvim/extras/fzf/tokyonight_moon.sh
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none
+  --color=bg+:#2d3f76 \
+  --color=bg:#1e2030 \
+  --color=border:#589ed7 \
+  --color=fg:#c8d3f5 \
+  --color=gutter:#1e2030 \
+  --color=header:#ff966c \
+  --color=hl+:#65bcff \
+  --color=hl:#65bcff \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#65bcff \
+  --color=query:#c8d3f5:regular \
+  --color=scrollbar:#589ed7 \
+  --color=separator:#ff966c \
+  --color=spinner:#ff007c \
+"
 export FZF_DEFAULT_COMMAND="fd -H -E '.git'"
 export FZF_PREVIEW_ADVANCED=true
 export FZF_PREVIEW_WINDOW='right:65%:nohidden'
