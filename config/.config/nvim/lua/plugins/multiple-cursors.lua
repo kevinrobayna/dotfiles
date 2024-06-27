9fc537c5a7aeeaaafa3202ef05f6662036d33ca3
Kevin Robayna <me@kevinrobayna.com>
2024-06-27 11:03:36 +0100

    bye bye multiline support

--- config/.config/nvim/lua/plugins/multiple-cursors.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "brenton-leighton/multiple-cursors.nvim",
-  version = "*", -- Use the latest tagged version
-  opts = {}, -- This causes the plugin setup function to be called
-  keys = {
-    { "<C-L>", "<Cmd>MultipleCursorsAddDown<CR>" },
-    { "<C-B>", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" } },
-  },
-}
