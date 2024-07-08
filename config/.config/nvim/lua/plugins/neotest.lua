ef65ae6ce0a0f280e82056551509887793d55116
Kevin Robayna <me@kevinrobayna.com>
2024-07-08 22:49:01 +0100

    reindent config

--- config/.config/nvim/lua/plugins/neotest.lua
+++ config/.config/nvim/lua/plugins/neotest.lua
@@ -1,20 +1,18 @@
 return {
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/neotest-vim-test",
-      "stevearc/overseer.nvim",
-    },
-    opts = function(_, opts)
-      opts.adapters = vim.list_extend(opts.adapters or {}, {
-        require("neotest-vim-test")({
-          ignore_file_types = { "ruby", "go" },
-        }),
-      })
-
-      opts.consumers = vim.list_extend(opts.consumers or {}, {
-        overseer = require("neotest.consumers.overseer"),
-      })
-    end,
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/neotest-vim-test",
+    "stevearc/overseer.nvim",
   },
+  opts = function(_, opts)
+    opts.adapters = vim.list_extend(opts.adapters or {}, {
+      require("neotest-vim-test")({
+        ignore_file_types = { "ruby", "go" },
+      }),
+    })
+
+    opts.consumers = vim.list_extend(opts.consumers or {}, {
+      overseer = require("neotest.consumers.overseer"),
+    })
+  end,
 }
