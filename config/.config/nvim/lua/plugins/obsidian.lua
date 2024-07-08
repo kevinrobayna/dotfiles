b4926eefa15cade4aa718e5e383b4e04da9d8393
Kevin Robayna <me@kevinrobayna.com>
2024-07-08 22:34:32 +0100

    reindent file

--- config/.config/nvim/lua/plugins/obsidian.lua
+++ config/.config/nvim/lua/plugins/obsidian.lua
@@ -2,63 +2,61 @@ vim.cmd([[highlight CodeBlock guibg=none]])
 vim.cmd([[highlight Headline guibg=none]])
 vim.cmd([[highlight Dash guibg=#D19A66 gui=bold]])
 return {
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    keys = {
-      { "<leader>no", "<Cmd>ObsidianToday<CR>", desc = "Open Notes" },
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  keys = {
+    { "<leader>no", "<Cmd>ObsidianToday<CR>", desc = "Open Notes" },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "Notes",
+        path = "~/dev/vault",
+      },
     },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "diary",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-W%W",
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "weekly.md",
     },
-    opts = {
-      workspaces = {
-        {
-          name = "Notes",
-          path = "~/dev/vault",
-        },
-      },
-      daily_notes = {
-        -- Optional, if you keep daily notes in a separate directory.
-        folder = "diary",
-        -- Optional, if you want to change the date format for the ID of daily notes.
-        date_format = "%Y-W%W",
-        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-        template = "weekly.md",
-      },
 
-      -- Optional, determines how certain commands open notes. The valid options are:
-      -- 1. "current" (the default) - to always open in the current window
-      -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
-      -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
-      open_notes_in = "vsplit",
+    -- Optional, determines how certain commands open notes. The valid options are:
+    -- 1. "current" (the default) - to always open in the current window
+    -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+    -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+    open_notes_in = "vsplit",
 
-      follow_url_func = function(url)
-        -- Open the URL in the default web browser.
-        vim.fn.jobstart({ "open", url }) -- Mac OS
-      end,
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.fn.jobstart({ "open", url }) -- Mac OS
+    end,
 
-      -- Where to put new notes created from completion. Valid options are
-      --  * "current_dir" - put new notes in same directory as the current buffer.
-      --  * "notes_subdir" - put new notes in the default notes subdirectory.
-      new_notes_location = "notes_subdir",
-      -- Either 'wiki' or 'markdown'.
-      preferred_link_style = "markdown",
+    -- Where to put new notes created from completion. Valid options are
+    --  * "current_dir" - put new notes in same directory as the current buffer.
+    --  * "notes_subdir" - put new notes in the default notes subdirectory.
+    new_notes_location = "notes_subdir",
+    -- Either 'wiki' or 'markdown'.
+    preferred_link_style = "markdown",
 
-      completion = {
-        -- Set to false to disable completion.
-        nvim_cmp = true,
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
 
-        -- Trigger completion at 2 chars.
-        min_chars = 2,
-      },
-      templates = {
-        subdir = "Templates",
-        date_format = "%Y-%m-%d-%a",
-        time_format = "%H:%M",
-      },
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+    templates = {
+      subdir = "Templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
     },
   },
 }
