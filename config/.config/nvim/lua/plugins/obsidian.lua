845b85b2c477520aa020ee7038f10e9790685b7c
Kevin Robayna <me@kevinrobayna.com>
2024-07-25 11:30:14 +0100

    fix bg for the markdown code block

--- config/.config/nvim/lua/plugins/obsidian.lua
+++ config/.config/nvim/lua/plugins/obsidian.lua
@@ -1,59 +1,77 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
-  ft = "markdown",
-  keys = {
-    { "<leader>no", "<Cmd>ObsidianToday<CR>", desc = "Open Notes" },
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "Notes",
-        path = "~/dev/vault",
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    opts = {
+      code = {
+        -- NOTE: Remove borders of code blocks
+        above = "",
+        below = "",
       },
     },
-    daily_notes = {
-      -- Optional, if you keep daily notes in a separate directory.
-      folder = "diary",
-      -- Optional, if you want to change the date format for the ID of daily notes.
-      date_format = "%Y-W%W",
-      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = "weekly.md",
-    },
-
-    -- Optional, determines how certain commands open notes. The valid options are:
-    -- 1. "current" (the default) - to always open in the current window
-    -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
-    -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
-    open_notes_in = "vsplit",
-
-    follow_url_func = function(url)
-      -- Open the URL in the default web browser.
-      vim.fn.jobstart({ "open", url }) -- Mac OS
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      -- NOTE: This needs to be after the setup
+      vim.cmd([[highlight RenderMarkdownCode guibg=none]])
+      vim.cmd([[highlight RenderMarkdownCodeBorder guibg=none]])
     end,
-
-    -- Where to put new notes created from completion. Valid options are
-    --  * "current_dir" - put new notes in same directory as the current buffer.
-    --  * "notes_subdir" - put new notes in the default notes subdirectory.
-    new_notes_location = "notes_subdir",
-    -- Either 'wiki' or 'markdown'.
-    preferred_link_style = "markdown",
-
-    completion = {
-      -- Set to false to disable completion.
-      nvim_cmp = true,
-
-      -- Trigger completion at 2 chars.
-      min_chars = 2,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    keys = {
+      { "<leader>no", "<Cmd>ObsidianToday<CR>", desc = "Open Notes" },
     },
-    templates = {
-      subdir = "Templates",
-      date_format = "%Y-%m-%d-%a",
-      time_format = "%H:%M",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Notes",
+          path = "~/dev/vault",
+        },
+      },
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "diary",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = "%Y-W%W",
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = "weekly.md",
+      },
+
+      -- Optional, determines how certain commands open notes. The valid options are:
+      -- 1. "current" (the default) - to always open in the current window
+      -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+      -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+      open_notes_in = "vsplit",
+
+      follow_url_func = function(url)
+        -- Open the URL in the default web browser.
+        vim.fn.jobstart({ "open", url }) -- Mac OS
+      end,
+
+      -- Where to put new notes created from completion. Valid options are
+      --  * "current_dir" - put new notes in same directory as the current buffer.
+      --  * "notes_subdir" - put new notes in the default notes subdirectory.
+      new_notes_location = "notes_subdir",
+      -- Either 'wiki' or 'markdown'.
+      preferred_link_style = "markdown",
+
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
+      templates = {
+        subdir = "Templates",
+        date_format = "%Y-%m-%d-%a",
+        time_format = "%H:%M",
+      },
     },
   },
 }
