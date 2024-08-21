a8f1db625f773199b200bb67f2f030532c74f035
Kevin Robayna <me@kevinrobayna.com>
2024-08-21 14:07:30 +0100

    include current session when looking for recent files
    I don't use tabs so i really want this to look in the things i'm
    currently looking at too

--- config/.config/nvim/lua/plugins/fzf.lua
+++ config/.config/nvim/lua/plugins/fzf.lua
@@ -2,6 +2,13 @@ return {
   "ibhagwan/fzf-lua",
   keys = {
     { "<leader>fR", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
-    { "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
+    {
+      "<leader>fr",
+      LazyVim.pick("oldfiles", {
+        cwd = vim.uv.cwd(),
+        include_current_session = true,
+      }),
+      desc = "Recent (cwd)",
+    },
   },
 }
