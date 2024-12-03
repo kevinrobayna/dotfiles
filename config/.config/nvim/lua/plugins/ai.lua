fe1a3225fb235993d94d628aea29f91f860be3af
Kevin Robayna <me@kevinrobayna.com>
2024-12-03 20:31:17 +0000

    use copilot everywhere

--- config/.config/nvim/lua/plugins/ai.lua
+++ config/.config/nvim/lua/plugins/ai.lua
@@ -3,16 +3,16 @@ local Util = require("lazyvim.util")
 return {
   {
     "zbirenbaum/copilot.lua",
-    cond = function()
-      local cwd = Util.root.cwd()
-      return string.match(cwd, "dev")
-    end,
+    --   cond = function()
+    --     local cwd = Util.root.cwd()
+    --     return string.match(cwd, "dev")
+    --   end,
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    cond = function()
-      local cwd = Util.root.cwd()
-      return string.match(cwd, "dev")
-    end,
+    -- cond = function()
+    --   local cwd = Util.root.cwd()
+    --   return string.match(cwd, "dev")
+    -- end,
   },
 }
