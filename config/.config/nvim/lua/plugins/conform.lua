f3b99020d52344c293be609931ab2ee4efd365f6
Kevin Robayna <me@kevinrobayna.com>
2025-02-06 14:57:09 +0000

    format jsonl files with jq

--- config/.config/nvim/lua/plugins/conform.lua
+++ config/.config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
     formatters_by_ft = {
       xml = { "xmlformat" },
       json = { "jq" },
+      jsonl = { "jq" },
       toml = { "taplo" },
       yaml = { "yq" },
       lua = { "stylua" },
