3a507bdfc51b6f0d4480708189c0a2aeaa8ed5e5
Kevin Robayna <me@kevinrobayna.com>
2025-04-07 18:19:26 +0100

    patch ai config

--- config/.config/nvim/lua/plugins/ai.lua
+++ config/.config/nvim/lua/plugins/ai.lua
@@ -3,6 +3,11 @@ local Util = require("lazyvim.util")
 return {
   {
     "zbirenbaum/copilot.lua",
+    -- FIXME: this needs to be removed soon :tm: just waiting for folke to merge
+    -- https://github.com/LazyVim/LazyVim/pull/5900
+    opts = function()
+      require("copilot.api").status = require("copilot.status")
+    end,
     --   cond = function()
     --     local cwd = Util.root.cwd()
     --     return string.match(cwd, "dev")
