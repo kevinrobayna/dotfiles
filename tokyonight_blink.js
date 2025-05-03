a310afcca69e236af4451748aa0406df87ceff98
Kevin Robayna <me@kevinrobayna.com>
2025-05-03 22:48:13 +0100

    generated from my alacritty's

---
+++ tokyonight_blink.js
@@ -0,0 +1,40 @@
+// TokyoNight-inspired terminal colors
+var black = "#1b1d2b";
+var red = "#ff757f"; // red
+var green = "#c3e88d"; // green
+var yellow = "#ffc777"; // yellow
+var blue = "#82aaff"; // blue
+var magenta = "#c099ff"; // magenta
+var cyan = "#86e1fc"; // cyan
+var white = "#828bb8"; // white
+var lightBlack = "#444a73"; // bright black
+var lightRed = "#ff757f"; // bright red
+var lightGreen = "#c3e88d"; // bright green
+var lightYellow = "#ffc777"; // bright yellow
+var lightBlue = "#82aaff"; // bright blue
+var lightMagenta = "#c099ff"; // bright magenta
+var lightCyan = "#86e1fc"; // bright cyan
+var lightWhite = "#c8d3f5"; // bright white
+
+term_set("color-palette-overrides", [
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+  lightBlack,
+  lightRed,
+  lightGreen,
+  lightYellow,
+  lightBlue,
+  lightMagenta,
+  lightCyan,
+  lightWhite,
+]);
+
+term_set("cursor-color", "#c8d3f5");
+term_set("foreground-color", "#c8d3f5");
+term_set("background-color", "#222436");
