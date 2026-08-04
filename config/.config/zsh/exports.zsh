7a2935bf0fff148511fc1ce5c04e2e04e0df1439
Kevin Robayna <me@kevinrobayna.com>
2026-08-04 13:27:36 +0100

    update model

--- config/.config/zsh/exports.zsh
+++ config/.config/zsh/exports.zsh
@@ -88,7 +88,7 @@ export AWS_SESSION_TTL="12h"
 export DD_SITE=datadoghq.eu
 
 # Preferred model
-export ANTHROPIC_MODEL="claude-opus-4-8[1m]"
+export ANTHROPIC_MODEL="claude-opus-5[1m]"
 
 # Configure Colima as Docker Host for applications which don't respect Docker Contexts
 # Testcontainers should use the default Docker Socket
